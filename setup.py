48e997d32155abd35fc0450d8da8458d2d151891
Jim Robinson <jscrobinson@gmail.com>
2020-12-01 14:09:31 +0000

    Add recursive values parsing

--- setup.py
+++ setup.py
@@ -13,11 +13,18 @@ from pkg_resources import VersionConflict, require
 from setuptools import setup
 
 try:
-    require('setuptools>=38.3')
+    require("setuptools>=38.3")
 except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup(
+        use_pyscaffold=True,
+        entry_points={
+            "console_scripts": [
+                "croudtech-lambda-helper=croudtech_python_aws_lambda_helpers.cli:cli"
+            ],
+        },
+    )
